c98a570d8a2e76758467e5b9c9495107e0a97a9d
YifengGao <yifeng@jiacz.com>
2019-06-20 01:20:27 +0800

    任务11：数据传输对象（DTO）

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Req, Query, Headers, Param, Post, Body } from '@nestjs/common';
 
+import { CreatePostDto } from './post.dto';
+
 @Controller('posts')
 export class PostsController {
     @Get()
     index(@Headers('authorization') headers) {
-        console.log(headers)
+        console.log(headers);
 
         return [
             {
@@ -13,15 +15,15 @@ export class PostsController {
         ];
     }
 
-    
     @Get(':id')
     show(@Param() params) {
         return {
             title: `Post ${params.id}`
-        }
+        };
     }
+
     @Post()
-    store(@Body() body) {
-        console.log(body)
+    store(@Body() post: CreatePostDto) {
+        console.log(post.title);
     }
 }
